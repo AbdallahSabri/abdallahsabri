74ad3a46f5302cf23f60a85cce1f0cc80dd2461a
abdallah <abdallah.silwad@gmail.com>
2026-05-04 00:07:33 +0300

    Fix localeCookie so the social crowlers can work fine

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,6 +4,7 @@ import { createNavigation } from "next-intl/navigation";
 export const routing = defineRouting({
   locales: ["en", "ar"],
   defaultLocale: "en",
+  localeCookie: false,
 });
 
 export const { Link, redirect, usePathname, useRouter } =
